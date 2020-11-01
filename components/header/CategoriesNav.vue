f481eb10ae328a521f3b9c2b481bd40111d2a80c
Suraiya@AndIT <webguru071@gmail.com>
2020-11-01 15:42:05 +0600

    CategoryNav added in Home Page

--- components/header/CategoriesNav.vue
+++ components/header/CategoriesNav.vue
@@ -3,7 +3,9 @@
   <h5 class="text-uppercase rounded-top">Categories</h5>
     <!-- Navigation -->
     <b-nav vertical>
-      <b-nav-item to="/categories/category-list">Ocean Food<i class="fas fa-angle-right"></i></b-nav-item>
+      <b-nav-item to="/categories/category-list" v-for="item in navData.data" :key="item.id">{{item.name}}<i class="fas fa-angle-right"></i></b-nav-item>
+
+      <!-- <b-nav-item to="/categories/category-list">Ocean Food<i class="fas fa-angle-right"></i></b-nav-item>
       <b-nav-item to="/categories/category-list">Butter & Eggs<i class="fas fa-angle-right"></i></b-nav-item>
       <b-nav-item to="/categories/category-list">Dried Fruits<i class="fas fa-angle-right"></i></b-nav-item>
       <b-nav-item to="/categories/category-list">Fast Food<i class="fas fa-angle-right"></i></b-nav-item>
@@ -11,15 +13,26 @@
       <b-nav-item to="/categories/category-list">Fruits<i class="fas fa-angle-right"></i></b-nav-item>
       <b-nav-item to="/categories/category-list">Milk & Cream<i class="fas fa-angle-right"></i></b-nav-item>
       <b-nav-item to="/categories/category-list">Vegetables<i class="fas fa-angle-right"></i></b-nav-item>
-      <b-nav-item to="/categories/category-list">Prime Video<i class="fas fa-angle-right"></i></b-nav-item>
+      <b-nav-item to="/categories/category-list">Prime Video<i class="fas fa-angle-right"></i></b-nav-item> -->
   </b-nav>
   <h6><nuxt-link to="/categories/"><b-icon-plus></b-icon-plus> More Categories</nuxt-link></h6>
   </div>
 </template>
 
 <script>
+import { mapGetters,mapActions } from 'vuex'
 export default {
-
+  computed: {
+    ...mapGetters({navData : 'category/allCategories'}),
+  },
+  methods: {
+    ...mapActions({
+        cat : 'category/getCategories',
+      }),
+  },
+  created(){
+      this.cat();
+  },
 }
 </script>
 
