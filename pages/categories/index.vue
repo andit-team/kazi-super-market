f727feac014ae4df725d4d9ac1cfe6694a3893d5
Suraiya@AndIT <webguru071@gmail.com>
2020-11-05 17:19:03 +0600

    Sub Category in Category List page done & Register and Login page doing

--- pages/categories/index.vue
+++ pages/categories/index.vue
@@ -2,16 +2,16 @@
   <div>
     <BannerHomeSlider />
     <!-- <BannerPage :bannerImg="bannerImg" /> -->
-    <Breadcrumbs title="All Category" />
+    <Breadcrumbs title="All Categories" />
     <!-- Area Start -->
     <section class="categories-page section-b-space">
       <div class="container">
 
         <div class="row categories-wrap">
-          <div class="col-md-6 col-lg-3 col-sm-12" v-for="categoryItem in categories" :key="categoryItem.id">
+          <!-- <b-nav-item to="/categories/category-list" v-for="categoryItem in categoryData" :key="categoryItem.id">{{categoryItem}}<i class="fas fa-angle-right"></i></b-nav-item> -->
 
-            <CategoryItem :categoryItem = "categoryItem" :index="index" />
-
+          <div class="col-md-6 col-lg-3 col-sm-12" v-for="categoryItem in categoryData.data" :key="categoryItem.id">
+            <CategoryItem :categoryItem = "categoryItem" />
           </div>
         </div>
         <!-- Area End -->
@@ -22,52 +22,64 @@
 </template>
 
 <script>
-
+import { mapGetters,mapActions } from 'vuex'
 export default {
   layout: 'public',
-  
   data() {
     return {
       bannerImg: require('@/assets/images/banner-img/addvartise-1.jpg'),
-      title: 'Explore this weeks specials',
-    categories:[
-          { 
-          id: 1,
-          title: "Freezer",
-          subTitle: "Patties Party Pack 1.25kg, Della Rosa Pizza 500g",
-          offerImage: require('@/assets/images/product-img/offer.svg'),
-          productImage: require('@/assets/images/product-img/product-1.png'),
-        },
-          { 
-          id: 2,
-          title: "Freezer",
-          subTitle: "Patties Party Pack 1.25kg, Della Rosa Pizza 500g",
-          offerImage: require('@/assets/images/product-img/offer.svg'),
-          productImage: require('@/assets/images/product-img/product-2.png'),
-        },
-          { 
-          id: 3,
-          title: "Freezer",
-          subTitle: "Patties Party Pack 1.25kg, Della Rosa Pizza 500g",
-          offerImage: require('@/assets/images/product-img/offer.svg'),
-          productImage: require('@/assets/images/product-img/product-3.png'),
-        },
-          { 
-          id: 4,  
-          title: "Freezer",
-          subTitle: "Patties Party Pack 1.25kg, Della Rosa Pizza 500g",
-          offerImage: require('@/assets/images/product-img/offer.svg'),
-          productImage: require('@/assets/images/product-img/product-4.png'),
-        },
-    ],
+      // title: 'Explore this weeks specials',
+      
+      // categories:[
+      //       { 
+      //       id: 1,
+      //       title: "Freezer",
+      //       subTitle: "Patties Party Pack 1.25kg, Della Rosa Pizza 500g",
+      //       offerImage: require('@/assets/images/product-img/offer.svg'),
+      //       productImage: require('@/assets/images/product-img/product-1.png'),
+      //     },
+      //       { 
+      //       id: 2,
+      //       title: "Freezer",
+      //       subTitle: "Patties Party Pack 1.25kg, Della Rosa Pizza 500g",
+      //       offerImage: require('@/assets/images/product-img/offer.svg'),
+      //       productImage: require('@/assets/images/product-img/product-2.png'),
+      //     },
+      //       { 
+      //       id: 3,
+      //       title: "Freezer",
+      //       subTitle: "Patties Party Pack 1.25kg, Della Rosa Pizza 500g",
+      //       offerImage: require('@/assets/images/product-img/offer.svg'),
+      //       productImage: require('@/assets/images/product-img/product-1.png'),
+      //     },
+      //       { 
+      //       id: 4,  
+      //       title: "Freezer",
+      //       subTitle: "Patties Party Pack 1.25kg, Della Rosa Pizza 500g",
+      //       offerImage: require('@/assets/images/product-img/offer.svg'),
+      //       productImage: require('@/assets/images/product-img/product-2.png'),
+      //     },
+      // ],
 
     }
+  },
+
+  computed: {
+    ...mapGetters({categoryData : 'category/allCategories'}),
+  },
+  methods: {
+    ...mapActions({
+        cat : 'category/getCategories',
+      }),
+  },
+  created(){
+      this.cat();
   },
 
 }
 </script>
 <style>
-  .categories-page .category-box .offer-tile-header {
+  /* .categories-page .category-box .offer-tile-header {
     display: none !important;
-  }
+  } */
 </style>
