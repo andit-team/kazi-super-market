9cdbd2eb831df5f226205aab55e6b624c1a00256
Shariful Islam <shariful.info56@gmail.com>
2020-12-08 11:45:17 +0600

    search running

--- store/product.js
+++ store/product.js
@@ -66,7 +66,7 @@ const actions = {
             meta_keyword : payload.meta_keyword,
             meta_description : payload.meta_description
         }
-        const response = await axios.post(process.env.API_URL+'/product/create/',data);
+        const response = await axios.post(process.env.API_URL+'/product/create/',data,helper.AuthHeader());
         return response.data
     },
 
@@ -119,8 +119,9 @@ const actions = {
         return response.data.data[0]
     },
 
-    async categoryWiseProduct({commit},cat_slug){
-        const response = await axios.get(process.env.API_URL+'/'+cat_slug+'/products');
+    async categoryWiseProduct({commit},payload){
+        console.log(payload);
+        const response = await axios.post(process.env.API_URL+'/product/search',payload);
         commit('SET_CATEGORY_WISE_PRODUCT',response.data.data)
         return response.data.data
     },
